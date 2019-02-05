fcb542e1a26eb6d878102f2e648bfb73d3d988d5
bestendeavors <43024623+bestendeavors@users.noreply.github.com>
2019-02-04 23:28:07 -0800

    Create custom.js

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $('.about-link').click(function(){
+    $('.about-link').fadeOut(200);
+    $('.title').fadeOut(200);    
+    $('.close-link').fadeIn(200); 
+    $('#responsive').fadeIn(1000);
+    console.log('eee');
+  });
+
+  $('.close-link').click(function(){
+    $('.close-link').fadeOut(200);
+    $('#responsive').fadeOut(200);    
+    $('.about-link').fadeIn(200);
+    $('.title').fadeIn(1000);
+
+  });
+
+  var fadeShow = $(".background").fadeShow({
+    correctRatio: true,
+    shuffle: false,
+    speed: 6000,
+    images: ['img/slider-background-1.jpg',
+            'img/slider-background-2.jpg',
+            'img/slider-background-3.jpg',
+            'img/slider-background-4.jpg']
+  }); 
+});    
